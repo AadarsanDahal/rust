01f9c52a84ff1c952d2329e35aa81e5f86bd3cd3
AadarsanDahal <aadarsandahal@gmail.com>
2024-12-29 23:06:20 +0545

    day 2 of rust completed and uploded

---
+++ main.rs
@@ -0,0 +1,50 @@
+ //steps to solve the problem
+// printing and formatting 
+
+fn main() {
+println!("hello this is day 2 of rust ");
+
+println!("my name is ? and i love robots ");
+let name = "Aarsan";
+//basic formatting 
+println!("my name is {} and i love robots ", name);
+println!("my name is {} and i love {} ", "Jhon", "glasses");
+println!("my name is {} and i love robots ", name);
+                       println!("{}",4);
+
+// for eg place holders
+
+      println!("my best frinend is {} and she is {} years old", "aaisha", 17);
+// i dont want to write name twice so i will try to use use 0 and 1 as counting
+println!("my best frinend is {0} and {0} is {1} years old","aaisha" ,17);
+
+// named arguments adding 
+
+println!("my name is {name} and i am {age}",name="Bandri", age=18);
+
+//tuples
+
+let student  =("aadarsan",19,true);
+println!(" my name is {:?} ",student);
+
+//basic mathmatics |o|
+println!("binary {:b}",5); // gives a binaryy tings 
+
+f numbers 
+let x = 12;
+let y = 3;
+let mut z=0;
+z=x+y;
+// just learned that sum cant be used llowl caues thats pre defeiced
+    println!("the sum is {z}");
+println!(" is {}",12+3);
+println!("the product are is {}",12*3);
+    println!("the product are is {}",12/3);
+println!("the product are is {}",12+3);
+println!("the product are is {}",12-3);
+
+// rust formatting
+
+} 
+ 
+
